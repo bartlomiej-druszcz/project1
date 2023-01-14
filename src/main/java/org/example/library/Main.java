7b7bcbb5d7065aef3d39563c91597a46908f986f
bartlomiej-druszcz <bartlomiej.druszcz@gmail.com>
2023-01-14 10:36:41 +0100

    Add User

--- src/main/java/org/example/library/Main.java
+++ src/main/java/org/example/library/Main.java
@@ -1,7 +1,11 @@
 package org.example.library;
 
+import org.example.library.model.User;
+
 public class Main {
     public static void main(String[] args) {
+        User user = new User("Bartek");
+        user.getName();
         String testCheck = "sd";
         System.out.println(testCheck);
     }
